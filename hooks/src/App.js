5a5986002fb6c99cb7f6d83c67a2f16573184ad0
adityash97 <adityash212@gmail.com>
2023-07-24 16:47:26 +0530

    simple useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,67 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
-
-  // states
-  const [postData, setpostData] = useState([]);
+  // states & hooks
   const [count, setCount] = useState(0);
-  const [state, setState] = useState({ count: 4, color: "red" });
-  const counter = state.count;
-  const color = state.color;
-
-  // hooks
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((data) => {
-        console.log("called");
-        setpostData(data.data);
-      })
-      .catch((err) => err);
-  }, []);
 
   // utility functions
-
-  function decCountby2() {
-    setCount((prevCount) => prevCount - 1);
-    setCount((prevCount) => prevCount - 1);
-  }
-  function incCountBy2() {
-    setCount((prevCount) => prevCount + 1);
-    setCount((prevCount) => prevCount + 1);
-  }
-
-  const incCounetr = () => {
-    setState((prevState) => {
-      return { ...prevState, count: prevState.count + 1 };
-    });
+  const addCount = () => {
+    setCount((prevCount) =>prevCount + 1);
+  };
+  const subCount = () => {
+    setCount((prev) => prev > 0 ? prev - 1 : prev);
   };
 
-  const decCounter = () => {
-    setState((prevState) => {
-      return { ...prevState, count: prevState.count - 1 };
-    });
-  };
-
+  
   return (
     <>
-      <button onClick={decCountby2}>-</button>
-      {count}
-      <button onClick={incCountBy2}>+</button>
-      <br />
-      <h6>Working with object in setState</h6>
-      <button onClick={decCounter}>-</button>
-      {counter} : {color}
-      <button onClick={incCounetr}>+</button>
-      <ul>
-        {postData.map((item, i) => {
-          return <li key={i}>{JSON.stringify(item)}</li>;
-        })}
-      </ul>
+      <div className=" container-fluid d-flex justify-content-arround justify-content-center align-items-center">
+        <button className="btn btn-primary" onClick={addCount}>
+          +
+        </button>
+        <h3>count : {count}</h3>
+        <button className="btn btn-warning" onClick={subCount}>
+          -
+        </button>
+      </div>
     </>
   );
 }
 
 export default App;
-
