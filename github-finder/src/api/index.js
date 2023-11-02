05dab039c2132c436441405e10b345f6374725cd
adityash97 <adityash212@gmail.com>
2023-11-02 19:22:47 +0530

    web search enabled , need to implement local storage

--- github-finder/src/api/index.js
+++ github-finder/src/api/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const BASE_URL = "https://api.github.com/users";
+// To get user match
+// https://api.github.com/search/users?q=user_id
 
 // To get user details (also followers count)
 // https://api.github.com/users/jsa
@@ -15,14 +17,14 @@ export async function getAllUsers(setUsers, setFilterUsers) {
   let res = await axios.get(BASE_URL);
   setUsers(res.data);
   setFilterUsers(res.data);
-//   console.log("data", res.data);
+  //   console.log("data", res.data);
 }
 // Getting details of a single user
 export async function getUserDetails(user_id, setuserdata) {
   await axios
     .get(BASE_URL + `/${user_id}`)
     .then((res) => {
-        setuserdata(res.data);
+      setuserdata(res.data);
     })
     .catch((error) => {
       return {};
@@ -40,13 +42,23 @@ export async function getRepos(user_id, setRepos) {
     });
 }
 
-export async function getFollowers(user_id,setFollowers){
-    await axios
-      .get(BASE_URL + `/${user_id}/followers`)
-      .then((res) => {
-        setFollowers(res.data);
-      })
-      .catch((error) => {
-        return {};
-      });
+export async function getFollowers(user_id, setFollowers) {
+  await axios
+    .get(BASE_URL + `/${user_id}/followers`)
+    .then((res) => {
+      setFollowers(res.data);
+    })
+    .catch((error) => {
+      return {};
+    });
+}
+export async function getUserMatch(user_id, setUsers, setFilterUsers) {
+  let res = await axios.get(`https://api.github.com/search/users?q=${user_id}`);
+  setUsers((oldData) => {
+    oldData = [...oldData, ...res.data.items];
+    return oldData;
+  });
+  setFilterUsers((oldData) => {
+    return (oldData = [...oldData, ...res.data.items]);
+  });
 }
